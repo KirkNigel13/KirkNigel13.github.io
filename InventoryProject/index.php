e86ceac2a51e7c2377153032d023b363c344381d
KirkNigel13 <kirknigel_13@yahoo.com>
2021-06-23 08:56:01 +0800

    uploading website

---
+++ InventoryProject/index.php
@@ -0,0 +1,109 @@
+<html>
+<head>
+    <title>InventoryProj | Homepage</title>
+
+    <!--  Mobile Compatibility  -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--  Bootstrap Injector  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!--  Custom Styles File  -->
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+    <!-- Link Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Top Header -->
+<nav class="navbar navbar-dark bg-dark">
+ 
+  <div class="container">
+    <a class="navbar-brand">Inventory Project</a>
+
+    <nav class="nav">
+    <a class="nav-link active-page" aria-current="page" href="#">Dashboard</a>
+    <a class="nav-link " href="inventory_list.php">Inventory List</a>
+    <a class="nav-link " href="#">Add New Item</a>
+    <a class="nav-link " href="#">User List</a>
+    </nav>
+
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+
+</nav>
+
+<!-- Main Content -->
+<div class="container pagecontent">
+    <!-- Header -->
+    <div class="padded">
+    <h2>Dashboard</h2>
+    </div>
+
+
+    <!-- Navigation Tabs -->
+        <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+
+        <button class="nav-link active col-4 tab-text" id="nav-active-tab" data-bs-toggle="tab" data-bs-target="#nav-active" type="button" role="tab" aria-controls="nav-active" aria-selected="true">
+            ACTIVE
+        </button>
+        
+
+        <button class="nav-link col-4 tab-text" id="nav-avail-tab" data-bs-toggle="tab" data-bs-target="#nav-avail" type="button" role="tab" aria-controls="nav-avail" aria-selected="false">
+            AVAILABLE
+        </button>
+        <button class="nav-link col-4 tab-text" id="nav-defective-tab" data-bs-toggle="tab" data-bs-target="#nav-defective" type="button" role="tab" aria-controls="nav-defective" aria-selected="false">
+            DEFECTIVE
+        </button>
+        
+        </nav>
+
+    <!-- Navigation Tab Contents -->
+   
+    <div class="tab-content" id="nav-tabContent">
+
+    <div class="tab-pane show active padded bg-white" id="nav-active" role="tabpanel" aria-labelledby="nav-active-tab">
+        <h4>Active</h4>
+
+
+
+
+    </div>
+
+    <div class="tab-pane padded bg-white" id="nav-avail" role="tabpanel" aria-labelledby="nav-avail-tab">
+    <h4>Available</h4>
+
+    
+
+    </div>
+
+    <div class="tab-pane padded bg-white" id="nav-defective" role="tabpanel" aria-labelledby="nav-defective-tab">
+    <h4>Defective</h4>
+
+    
+
+    </div>
+    
+
+
+
+
+</div>
+
+
+
+
+<!-- Bootstrap Scripts -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    </body>
+
+</html>
---
+++ InventoryProject/inventory_list.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+<title>InventoryProj | Inventory List</title>
+
+<!--  Mobile Compatibility  -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--  Bootstrap Injector  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<!--  Custom Styles File  -->
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+<!-- Link Font -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Top Header -->
+<nav class="navbar navbar-dark bg-dark">
+ 
+  <div class="container">
+    <a class="navbar-brand">Inventory Project</a>
+
+    <nav class="nav">
+    <a class="nav-link " href="index.php">Dashboard</a>
+    <a class="nav-link active-page" aria-current="page" href="#">Inventory List</a>
+    <a class="nav-link " href="#">Add New Item</a>
+    <a class="nav-link " href="#">User List</a>
+    </nav>
+
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+
+</nav>
+
+<!-- Main Content -->
+<div class="container pagecontent">
+    <!-- Header -->
+    <div class="padded">
+    <h2>Inventory List</h2>
+    </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Bootstrap Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>
+
+</html>
